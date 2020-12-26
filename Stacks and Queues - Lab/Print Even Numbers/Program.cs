33ef95a0e22b7354fba717a7b1f3808d77436183
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-26 22:47:13 +0200

    merry christmas ;]

---
+++ Stacks and Queues - Lab/Print Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            Queue<int> nums = new Queue<int>(numbers.Where(x => x % 2 == 0));
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/Supermarket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+
+            Queue<string> customers = new Queue<string>();
+
+            while ((name = Console.ReadLine()) != "End")
+            {
+                if(name == "Paid")
+                {
+                    for (int i = 0; i <= customers.Count; i++)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                        i = 0;
+                    }
+                }
+                else
+                {
+                    customers.Enqueue(name);
+                }
+            }
+
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
