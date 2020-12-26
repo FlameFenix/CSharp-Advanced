a178c8055b4dbaeb50783f11a57da4b4beb17c39
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-26 21:27:08 +0200

    merry christmas ;]

---
+++ Stacks and Queues - Lab/Reverse Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> reverse = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                reverse.Push(input[i]);
+            }
+
+            print(reverse);
+        }
+
+        public static void print(Stack<char> reverse)
+        {
+            foreach (var item in reverse)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/Stack Sum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            Stack<int> stackNumbers = new Stack<int>(numbers);
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                          .ToArray();
+                string currentCommand = cmdArgs[0];
+
+                if(currentCommand == "add")
+                {
+                    int numberOne = int.Parse(cmdArgs[1]);
+                    int numberTwo = int.Parse(cmdArgs[2]);
+
+                    stackNumbers.Push(numberOne);
+                    stackNumbers.Push(numberTwo);
+                }
+                else if(currentCommand == "remove")
+                {
+                    int removeCount = int.Parse(cmdArgs[1]);
+
+                    if(removeCount < stackNumbers.Count)
+                    {
+                        for (int i = 0; i < removeCount; i++)
+                        {
+                            stackNumbers.Pop();
+                        }
+                    }
+                }
+
+            }
+            Console.WriteLine($"Sum: {stackNumbers.Sum()}");
+        }
+    }
+}
