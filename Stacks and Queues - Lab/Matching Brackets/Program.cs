5feb98ff94efbc57788ab19b8405d1b02aae050b
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-26 22:28:47 +0200

    merry christmas ;]

---
+++ Stacks and Queues - Lab/Matching Brackets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Matching_Brackets
+{
+    class Program
+    {
+        private static object stack;
+
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if(expression[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if(expression[i] == ')')
+                {
+                    int start = stack.Pop();
+                    string result = expression.Substring(start, i - start + 1);
+                    Console.WriteLine(result);
+                }
+                
+            }
+            
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/Simple Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] numbers = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .Reverse()
+                                      .ToArray();
+
+            Stack<string> sum = new Stack<string>(numbers);
+
+            while (sum.Count != 1)
+            {
+                int numberOne = int.Parse(sum.Pop());
+                string separator = sum.Pop();
+                int numberTwo = int.Parse(sum.Pop());
+                if(separator == "+")
+                {
+                    string sumNumbers = (numberOne + numberTwo).ToString();
+                    sum.Push(sumNumbers);
+                }
+                else if(separator == "-")
+                {
+                    string sumNumbers = (numberOne - numberTwo).ToString();
+                    sum.Push(sumNumbers);
+                }
+                
+            }
+
+            foreach (var item in sum)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
