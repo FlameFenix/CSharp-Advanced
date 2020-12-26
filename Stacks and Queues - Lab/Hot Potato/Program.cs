9519d902eda3566966e6693508555f12bef2f0ee
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-26 23:10:32 +0200

    merry christmas ;]

---
+++ Stacks and Queues - Lab/Hot Potato/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .ToArray();
+
+            
+            Queue<string> team = new Queue<string>(names);
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            while (team.Count != 1)
+            {
+                for (int i = 1; i < rotations; i++)
+                {
+                    team.Enqueue(team.Dequeue());
+                }
+                Console.WriteLine($"Removed {team.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {string.Join(Environment.NewLine, team)}");
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/Traffic Jam/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            string command = string.Empty;
+
+            Queue<string> cars = new Queue<string>();
+
+            int countPassedCars = 0;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < numberOfCars; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            countPassedCars++;
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+            }
+            Console.WriteLine($"{countPassedCars} cars passed the crossroads.");
+        }
+    }
+}
