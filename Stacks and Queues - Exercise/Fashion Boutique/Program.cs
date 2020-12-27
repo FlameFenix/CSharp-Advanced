2ece6a865d1d5c04ae1204db7492e133c116bf1f
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-27 21:55:22 +0200

    merry christmas ;]

---
+++ Stacks and Queues - Exercise/Fashion Boutique/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequenceNumber = Console.ReadLine()
+                                          .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                          .Select(int.Parse)
+                                          .ToArray();
+
+            int target = int.Parse(Console.ReadLine());
+
+            Stack<int> stackNumbers = new Stack<int>(sequenceNumber);
+
+            int sumNumbers = 0;
+
+            int countRack = 0;
+
+            while (stackNumbers.Count > 0)
+            {
+                int currentNumber = stackNumbers.Peek();
+
+                if (sumNumbers + currentNumber > target)
+                {
+                    sumNumbers = 0;
+                    countRack++;
+                }
+                else if (sumNumbers + currentNumber == target)
+                {
+                    sumNumbers = 0;
+                    countRack++;
+                    stackNumbers.Pop();
+                }
+                else
+                {
+                    sumNumbers += stackNumbers.Pop();
+                }
+
+
+            }
+
+            if (sumNumbers != 0)
+            {
+                countRack++;
+            }
+
+            Console.WriteLine(countRack);
+
+        }
+    }
+}
---
+++ Stacks and Queues - Exercise/Songs Queue/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine()
+                                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                    .ToArray();
+
+            string command = string.Empty;
+
+            Queue<string> songsList = new Queue<string>(songs);
+
+            while (songsList.Count >= 0)
+            {
+                if (songsList.Count == 0)
+                {
+                    Console.WriteLine("No more songs!");
+                    break;
+                }
+
+                command = Console.ReadLine();
+
+                if (command == "Play")
+                {
+                    songsList.Dequeue();
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songsList));
+                }
+                else
+                {
+                    string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    if (cmdArgs[0] == "Add")
+                    {
+                        string currentSong = command.Substring(4, command.Length - 4);
+                        if (songsList.Contains(currentSong))
+                        {
+                            Console.WriteLine($"{currentSong} is already contained!");
+                        }
+                        else
+                        {
+                            songsList.Enqueue(currentSong);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
