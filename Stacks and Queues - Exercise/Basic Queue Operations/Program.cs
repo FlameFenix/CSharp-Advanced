bce2342cb3665cb684e24be14eedbd2ce13bc1ea
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-27 11:53:31 +0200

    merry christmas ;]

---
+++ Stacks and Queues - Exercise/Basic Queue Operations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int addNumbers = numbers[0];
+            int removeNumbers = numbers[1];
+            int existingNumber = numbers[2];
+
+            int[] sequence = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            Queue<int> queueNumbers = new Queue<int>();
+
+            for (int i = 0; i < addNumbers; i++)
+            {
+                queueNumbers.Enqueue(sequence[i]);
+            }
+
+            for (int i = 0; i < removeNumbers; i++)
+            {
+                queueNumbers.Dequeue();
+            }
+
+            if(queueNumbers.Contains(existingNumber))
+            {
+                Console.WriteLine(queueNumbers.Contains(existingNumber)
+                                              .ToString()
+                                              .ToLower());
+            }
+            else
+            {
+                if(queueNumbers.Count > 0)
+                {
+                    Console.WriteLine(queueNumbers.Min());
+                }
+                else
+                {
+                    Console.WriteLine("0");
+                }
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Exercise/Basic Stack Operations/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int nNumbers = numbers[0];
+            int popNumbers = numbers[1];
+            int existingElement = numbers[2];
+
+            int[] sequence = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            Stack<int> stackNumbers = new Stack<int>();
+
+            for (int i = 0; i < nNumbers; i++)
+            {
+                stackNumbers.Push(sequence[i]);
+            }
+
+            for (int i = 0; i < popNumbers; i++)
+            {
+                stackNumbers.Pop();
+            }
+
+            if(stackNumbers.Contains(existingElement))
+            {
+                Console.WriteLine(stackNumbers.Contains(existingElement)
+                                              .ToString()
+                                              .ToLower());
+            }
+            else
+            {
+                if(stackNumbers.Count > 0)
+                {
+                    Console.WriteLine(stackNumbers.Min());
+                }
+                else
+                {
+                    Console.WriteLine("0");
+                }
+               
+            }
+        }
+    }
+}
