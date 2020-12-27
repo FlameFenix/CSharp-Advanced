6f1892149e733b44bf63740697cefc309ecbd3bb
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-27 18:33:34 +0200

    merry christmas ;]

---
+++ Stacks and Queues - Exercise/Fast Food/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine()
+                                  .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            Queue<int> queueNums = new Queue<int>(orders);
+            string maxOrder = string.Empty;
+            if(queueNums.Count > 0)
+            {
+                maxOrder = queueNums.Max().ToString();
+            }
+            
+            while (queueNums.Count != 0 && foodQuantity - queueNums.Peek() >= 0)
+            {
+                if(foodQuantity - queueNums.Peek() >= 0)
+                {
+
+                    foodQuantity -= queueNums.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            if(queueNums.Count == 0)
+            {
+                Console.WriteLine(maxOrder);
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine(maxOrder);
+                Console.WriteLine($"Orders left: {string.Join(" ", queueNums)}");
+            }
+
+        }
+    }
+}
---
+++ Stacks and Queues - Exercise/Maximum and Minimum Element/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperionts = int.Parse(Console.ReadLine());
+
+            Stack<int> stackNumbers = new Stack<int>();
+
+            for (int i = 0; i < numberOfOperionts; i++)
+            {
+                int[] numberCommand = Console.ReadLine()
+                                             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                             .Select(int.Parse)
+                                             .ToArray();
+                int currentCommand = numberCommand[0];
+
+                if(currentCommand == 1) // Add
+                {
+                    int number = numberCommand[1];
+
+                    stackNumbers.Push(number);
+                }
+                else if(currentCommand == 2) // Delete
+                {
+                    if(stackNumbers.Count > 0)
+                    {
+                        stackNumbers.Pop();
+                    }
+                }
+                else if(currentCommand == 3) // Max element 
+                {
+                    if(stackNumbers.Count > 0)
+                    {
+                        Console.WriteLine(stackNumbers.Max());
+                    }
+                    
+                }
+                else if(currentCommand == 4) // Min element
+                {
+                    if(stackNumbers.Count > 0)
+                    {
+                        Console.WriteLine(stackNumbers.Min());
+                    }
+                }
+            }
+
+            if(stackNumbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", stackNumbers));
+            }
+            
+        }
+    }
+}
