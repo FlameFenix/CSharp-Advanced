b1dcb16d0516b35e930ea5b7465c2198ff6b782e
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-02 11:55:55 +0200

    happy new year ;p

---
+++ Multidimensional Arrays - Lab/Primary Diagonal/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            int diagonalSum = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                int[] numbers = Console.ReadLine()
+                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToArray();
+                
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = numbers[cols];
+                }
+            }
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    if(rows == cols)
+                    {
+                        diagonalSum += matrix[rows, cols];
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(diagonalSum);
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/Symbol in Matrix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[,] matrix = new string[n, n];
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                string chars = Console.ReadLine();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = chars[cols].ToString();
+                }
+            }
+
+            string character = Console.ReadLine();
+
+            bool isCharExist = false;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    if (character == matrix[rows, cols])
+                    {
+                        Console.WriteLine($"({rows}, {cols})");
+                        isCharExist = true;
+                        break;
+                    }
+                }
+                if(isCharExist)
+                {
+                    break;
+                }
+            }
+
+            if(!isCharExist)
+            {
+                Console.WriteLine($"{character} does not occur in the matrix");
+            }
+        }
+    }
+}
