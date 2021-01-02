cbd10bd9579e779f64e40ac31532b79293831b30
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-02 15:00:16 +0200

    happy new year ;p

---
+++ Multidimensional Arrays - Lab/Jagged-Array Modification/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[n][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                int[] numbers = Console.ReadLine()
+                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToArray();
+
+                matrix[row] = new int[numbers.Length];
+
+                for (int col = 0; col < numbers.Length; col++)
+                {
+                    matrix[row][col] = numbers[col];
+                }
+            }
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string currentCommand = cmdArgs[0];
+                int row = int.Parse(cmdArgs[1]);
+                int col = int.Parse(cmdArgs[2]);
+                int value = int.Parse(cmdArgs[3]);
+
+                bool isValueSet = false;
+
+                if (currentCommand == "Add")
+                {
+
+                    for (int rows = 0; rows < matrix.Length; rows++)
+                    {
+                        for (int cols = 0; cols < matrix[rows].Length; cols++)
+                        {
+                            if (row == rows && col == cols)
+                            {
+                                matrix[row][col] += value;
+                                isValueSet = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if(currentCommand == "Subtract")
+                {
+                    for (int rows = 0; rows < matrix.Length; rows++)
+                    {
+                        for (int cols = 0; cols < matrix[rows].Length; cols++)
+                        {
+                            if (row == rows && col == cols)
+                            {
+                                matrix[row][col] -= value;
+                                isValueSet = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if(!isValueSet)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+            }
+            for (int rows = 0; rows < matrix.Length; rows++)
+            {
+                for (int cols = 0; cols < matrix[rows].Length; cols++)
+                {
+                    Console.Write($"{matrix[rows][cols]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/Pascal Triangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRows = int.Parse(Console.ReadLine());
+
+            long[][] pascal = new long[numberOfRows][];
+            int cols = 1;
+
+            for (int rows = 0; rows < pascal.Length; rows++)
+            {
+                pascal[rows] = new long[cols];
+                pascal[rows][0] = 1;
+                pascal[rows][pascal[rows].Length - 1] = 1;
+                cols++;
+
+                if(rows > 1)
+                {
+                    for (int col = 1; col < pascal[rows].Length - 1; col++)
+                    {
+                        long[] prevRow = pascal[rows - 1];
+                        long firstNumber = prevRow[col];
+                        long secondNumber = prevRow[col - 1];
+                        pascal[rows][col] = firstNumber + secondNumber;
+                    }
+                }
+            }
+
+            for (int rows = 0; rows < pascal.Length; rows++)
+            {
+                for (int col = 0; col < pascal[rows].Length; col++)
+                {
+                    Console.Write(pascal[rows][col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
