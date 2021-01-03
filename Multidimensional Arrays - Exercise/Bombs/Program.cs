77ca6bd5aab8448f142c494c98a65af6c94298fc
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-03 18:40:38 +0200

    exercises

---
+++ Multidimensional Arrays - Exercise/Bombs/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+
+namespace Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeOfMatrix, sizeOfMatrix];
+
+            FillMatrix(matrix);
+
+            string[] bombs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            for (int i = 0; i < bombs.Length; i++)
+            {
+                int[] currentBomb = bombs[i].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                int bombRows = currentBomb[0];
+                int bombCols = currentBomb[1];
+
+                for (int rows = 0; rows < matrix.GetLength(0); rows++)
+                {
+                    for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                    {
+                        if (bombCols == cols && bombRows == rows)
+                        {
+                            if(matrix[rows, cols] > 0)
+                            {
+                                IsIndexExist(matrix, rows, cols);
+                            }
+                            
+                        }
+                    }
+                }
+            }
+
+            int countAlive = 0;
+            int sumAlive = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    if (matrix[rows, cols] > 0)
+                    {
+                        countAlive++;
+                        sumAlive += matrix[rows, cols];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {countAlive}");
+            Console.WriteLine($"Sum: {sumAlive}");
+            PrintMatrix(matrix);
+        }
+
+        private static void IsIndexExist(int[,] matrix, int rows, int cols)
+        {
+            int bomb = matrix[rows, cols];
+            
+            if (isValidIndex(matrix, rows - 1, cols) && matrix[rows - 1, cols] > 0)
+            {
+                    matrix[rows - 1, cols] -= bomb;
+            }
+            if (isValidIndex(matrix, rows - 1, cols - 1) && matrix[rows - 1, cols - 1] > 0)
+            {
+                    matrix[rows - 1, cols - 1] -= bomb;
+            }
+            if (isValidIndex(matrix, rows - 1, cols + 1) && matrix[rows - 1, cols + 1] > 0)
+            {
+                    matrix[rows - 1, cols + 1] -= bomb;
+            }
+
+            if (isValidIndex(matrix, rows + 1, cols) && matrix[rows + 1, cols] > 0)
+            {
+                    matrix[rows + 1, cols] -= bomb;
+            }
+            if (isValidIndex(matrix, rows + 1, cols + 1) && matrix[rows + 1, cols + 1] > 0)
+            {
+                    matrix[rows + 1, cols + 1] -= bomb;
+            }
+            if (isValidIndex(matrix, rows + 1, cols - 1) && matrix[rows + 1, cols - 1] > 0)
+            {
+                    matrix[rows + 1, cols - 1] -= bomb;
+            }
+
+            if (isValidIndex(matrix, rows, cols - 1) && matrix[rows, cols - 1] > 0)
+            {
+                    matrix[rows, cols - 1] -= bomb;
+            }
+            if (isValidIndex(matrix, rows, cols + 1) && matrix[rows, cols + 1] > 0)
+            {
+                    matrix[rows, cols + 1] -= bomb;
+            }
+            if (isValidIndex(matrix, rows, cols) && matrix[rows, cols] > 0)
+            {
+                matrix[rows, cols] -= bomb;
+            }
+        }
+
+        public static int[,] FillMatrix(int[,] matrix)
+        {
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                int[] numbers = Console.ReadLine()
+                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToArray();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = numbers[cols];
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.Write(matrix[rows, cols] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static bool isValidIndex(int[,] matrix, int rows, int cols)
+        {
+            return rows >= 0 && rows < matrix.GetLength(0)
+                && cols >= 0 && cols < matrix.GetLength(1);
+        }
+    }
+}
---
+++ Multidimensional Arrays - Exercise/Knight Game/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Linq;
+
+namespace Knight_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfField = int.Parse(Console.ReadLine());
+
+            char[,] chessBoard = new char[sizeOfField, sizeOfField];
+
+            FillMatrix(chessBoard);
+
+            
+            int countReplaced = 0;
+            int MaxAttacks = 0;
+
+            int attackerRow = 0;
+            int attackerCol = 0;
+
+            while (true)
+            {
+                for (int rows = 0; rows < chessBoard.GetLength(0); rows++)
+                {
+                    int countAttacks = 0;
+
+                    for (int cols = 0; cols < chessBoard.GetLength(1); cols++)
+                    {
+                        char currentPosition = chessBoard[rows, cols];
+                        if (currentPosition == 'K')
+                        {
+                            countAttacks = Attacks(chessBoard, rows, cols);
+                            if(countAttacks > MaxAttacks)
+                            {
+                                MaxAttacks = countAttacks;
+                                attackerRow = rows;
+                                attackerCol = cols;
+                            }
+                        }
+                    }
+                }
+
+                if(MaxAttacks > 0)
+                {
+                    chessBoard[attackerRow, attackerCol] = '0';
+                    MaxAttacks = 0;
+                    countReplaced++;
+                }
+                else
+                {
+                    Console.WriteLine(countReplaced);
+                    break;
+                }
+            }
+        }
+
+        private static int Attacks(char[,] chessBoard, int rows, int cols)
+        {
+            int countAttacks = 0;
+
+            if (indexValidation(chessBoard, rows - 2, cols - 1) == true)
+            {
+                if (chessBoard[rows - 2, cols - 1] == 'K')
+                {
+                    countAttacks++;
+                }
+            }
+            if (indexValidation(chessBoard, rows - 2, cols + 1) == true)
+            {
+                if (chessBoard[rows - 2, cols + 1] == 'K')
+                {
+                    countAttacks++;
+                }
+
+            }
+            if (indexValidation(chessBoard, rows - 1, cols - 2) == true)
+            {
+                if (chessBoard[rows - 1, cols - 2] == 'K')
+                {
+                    countAttacks++;
+                }
+            }
+            if (indexValidation(chessBoard, rows - 1, cols + 2) == true)
+            {
+                if (chessBoard[rows - 1, cols + 2] == 'K')
+                {
+                    countAttacks++;
+                }
+            }
+            if (indexValidation(chessBoard, rows + 1, cols - 2) == true)
+            {
+                if (chessBoard[rows + 1, cols - 2] == 'K')
+                {
+                    countAttacks++;
+                }
+            }
+            if (indexValidation(chessBoard, (rows + 1), (cols + 2)) == true)
+            {
+                if (chessBoard[rows + 1, cols + 2] == 'K')
+                {
+                    countAttacks++;
+                }
+
+            }
+            if (indexValidation(chessBoard, rows + 2, cols + 1) == true)
+            {
+                if (chessBoard[rows + 2, cols + 1] == 'K')
+                {
+                    countAttacks++;
+                }
+            }
+            if (indexValidation(chessBoard, rows + 2, cols - 1) == true)
+            {
+                if (chessBoard[rows + 2, cols - 1] == 'K')
+                {
+                    countAttacks++;
+                }
+            }
+
+            return countAttacks;
+        }
+
+        public static bool indexValidation(char[,] chessboard, int row, int col)
+        {
+            if (row >= 0 && row < chessboard.GetLength(0) &&
+                col >= 0 && col < chessboard.GetLength(1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static char[,] FillMatrix(char[,] matrix)
+        {
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                string characters = Console.ReadLine();
+                char[] charArray = characters.ToCharArray();
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+
+                    matrix[rows, cols] = charArray[cols];
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void PrintMatrix(char[,] matrix)
+        {
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.Write(matrix[rows, cols] + "");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
