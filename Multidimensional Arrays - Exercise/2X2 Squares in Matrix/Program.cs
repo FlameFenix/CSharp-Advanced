e7cda8baa7992d814a8498daa43069c51df261f4
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-03 11:26:33 +0200

    exercises

---
+++ Multidimensional Arrays - Exercise/2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace _2X2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                                      .Split()
+                                      .Select(int.Parse)
+                                      .ToArray();
+
+            char[,] matrix = new char[matrixSize[0], matrixSize[1]];
+
+            FillMatrix(matrix);
+
+            int counter = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    if(rows + 1 < matrix.GetLength(0)
+                    && cols + 1 < matrix.GetLength(1))
+                    {
+                        char firstChar = matrix[rows, cols];
+                        char secondChar = matrix[rows, cols + 1];
+                        char thirdChar = matrix[rows + 1, cols];
+                        char fourthChar = matrix[rows + 1, cols + 1];
+
+                        if (firstChar == secondChar
+                           && firstChar == thirdChar
+                           && firstChar == fourthChar)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+
+        public static char[,] FillMatrix(char[,] matrix)
+        {
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                char[] numbers = Console.ReadLine()
+                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(char.Parse)
+                                       .ToArray();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = numbers[cols];
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.Write(matrix[rows, cols] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Multidimensional Arrays - Exercise/Diagonal Difference/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            FillMatrix(matrix);
+
+            int sumD1 = 0;
+            int sumD2 = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    if(rows == cols)
+                    {
+                        sumD1 += matrix[rows, cols];
+                    }
+
+                    if(cols == n - 1 - rows)
+                    {
+                        sumD2 += matrix[rows, cols];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumD2 - sumD1));
+
+          //  PrintMatrix(matrix);
+        }
+
+        public static int[,] FillMatrix(int[,] matrix)
+        {
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                int[] numbers = Console.ReadLine()
+                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToArray();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = numbers[cols];
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.Write(matrix[rows, cols] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
