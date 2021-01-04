a705beb6ecbdb74b3bbabb233376838bee4bc9cc
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-04 21:24:37 +0200

    lab

---
+++ Sets and Dictionaries Advanced - Lab/Cities by Continent and Country/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfContinents = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continentsCountriesCities = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < numberOfContinents; i++)
+            {
+
+                string[] input = Console.ReadLine()
+                                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                        .ToArray();
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!continentsCountriesCities.ContainsKey(continent) )
+                {
+                    continentsCountriesCities.Add(continent, new Dictionary<string, List<string>>());
+                    
+                }
+
+                if(!continentsCountriesCities[continent].ContainsKey(country))
+                {
+                    continentsCountriesCities[continent].Add(country, new List<string>());
+                }
+                
+                continentsCountriesCities[continent][country].Add(city);
+            }
+
+            foreach (var continent in continentsCountriesCities)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var county in continent.Value)
+                {
+                    Console.WriteLine($"{county.Key} -> {string.Join(", ", county.Value)}");
+                }
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Lab/Product Shop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            while (input != "Revision")
+            {
+                string[] cmdArgs = input.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                        .ToArray();
+
+                string shop = cmdArgs[0];
+                string food = cmdArgs[1];
+                double price = double.Parse(cmdArgs[2]);
+
+                if(!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+
+                shops[shop].Add(food, price);
+
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in shops.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key}->");
+                foreach (var product in item.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
