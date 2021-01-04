a6943ebb1d7de854f957fd53f31c443c0f077730
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-04 21:35:36 +0200

    lab

---
+++ Sets and Dictionaries Advanced - Lab/Parking Lot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            HashSet<string> parking = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] cmdArgs = input.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                        .ToArray();
+
+                string option = cmdArgs[0];
+                string number = cmdArgs[1];
+                if(option == "IN")
+                {
+                    parking.Add(number);
+                }
+                else if(option == "OUT")
+                {
+                    parking.Remove(number);
+                }
+
+                input = Console.ReadLine();
+            }
+            if(parking.Count <= 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            foreach (var cars in parking)
+            {
+                Console.WriteLine(cars);
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Lab/Record Unique Names/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfNames = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>(numberOfNames);
+
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                string name = Console.ReadLine();
+
+                names.Add(name);
+            }
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
