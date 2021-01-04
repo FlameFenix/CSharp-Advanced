7f92bd3e840e6dfa4b7c076eaf3bf7babe329e61
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-04 20:59:18 +0200

    lab

---
+++ Sets and Dictionaries Advanced - Lab/1. Count Same Values in Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(!dictionary.ContainsKey(array[i]))
+                {
+                    dictionary.Add(array[i], 0);
+                }
+
+                dictionary[array[i]] += 1;
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Lab/Average Student Grades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentsGrade = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string[] studentInfo = Console.ReadLine()
+                                              .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                              .ToArray();
+                string name = studentInfo[0];
+                decimal grade = decimal.Parse(studentInfo[1]);
+
+                if (!studentsGrade.ContainsKey(name))
+                {
+                    studentsGrade.Add(name, new List<decimal>());
+                }
+
+                studentsGrade[name].Add(grade);
+
+            }
+
+            foreach (var item in studentsGrade)
+            {
+                Console.WriteLine($"{item.Key} -> {string.Join(" ", item.Value.Select(x => string.Format("{0:f2}", x)))} (avg: {item.Value.Average():f2})");
+            }
+        }
+    }
+}
