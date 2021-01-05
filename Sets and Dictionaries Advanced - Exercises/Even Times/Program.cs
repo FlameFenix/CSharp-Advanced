5607ff786b51d25a94e7239741f31bd65a2b1636
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-05 21:21:01 +0200

    exercises

---
+++ Sets and Dictionaries Advanced - Exercises/Even Times/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if(!numbers.ContainsKey(number))
+                {
+                    numbers.Add(number, 0);
+                }
+
+                numbers[number]++;
+            }
+
+            foreach (var item in numbers.Where(x => x.Value % 2 == 0))
+            {
+                Console.WriteLine(item.Key);
+                break;
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Exercises/Periodic Table/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+
+            HashSet<string> something = new HashSet<string>();
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                string[] input = Console.ReadLine()
+                                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                        .ToArray();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    something.Add(input[j]);
+                }
+            }
+
+            foreach (var item in something.OrderBy(x => x))
+            {
+                Console.Write($"{item} ");
+            }
+
+        }
+
+    }
+}
