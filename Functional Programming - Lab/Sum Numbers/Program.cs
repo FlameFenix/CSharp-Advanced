7910f713ee76a1b0d7dc56acabc2895eea9c112e
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-21 00:40:16 +0200

    lab

---
+++ Functional Programming - Lab/Sum Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            Func<int[], int> func = sumNumbers;
+            Func<int[], int> count = countNumbers;
+
+            Console.WriteLine(count(numbers));
+            Console.WriteLine(func(numbers));
+            
+        }
+
+        static int sumNumbers(int[] numbers)
+        {
+            int sum = numbers.Sum();
+
+            return sum;
+        }
+
+        static int countNumbers(int[] numbers)
+        {
+            int count = numbers.Length;
+            return count;
+        }
+    }
+}
