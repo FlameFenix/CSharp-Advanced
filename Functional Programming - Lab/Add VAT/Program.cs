dddae9970b64c8c1e59c6d7606a87c8eb7a1450b
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-21 22:33:27 +0200

    lab

---
+++ Functional Programming - Lab/Add VAT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] prices = Console.ReadLine()
+                                      .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(decimal.Parse)
+                                      .ToArray();
+
+            Func<decimal[], decimal[]> vat = AddVat;
+
+            foreach (var item in vat(prices))
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+
+        public static decimal[] AddVat(decimal[] prices)
+        {
+            for (int i = 0; i < prices.Length; i++)
+            {
+                prices[i] *= 1.2m;
+            }
+
+            return prices;
+        }
+    }
+}
---
+++ Functional Programming - Lab/Count Uppercase Words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Where(x => x[0] == x.ToUpper()[0])
+                                    .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
