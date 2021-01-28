0296d7d20cb0d5d5d993f8e1655c05181f839d90
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-28 22:41:22 +0200

    Exercises

---
+++ Functional Programming - Exercise/Action Print/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .ToArray();
+
+            Action<string[]> print =
+                names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            print(names);
+        }
+    }
+}
---
+++ Functional Programming - Exercise/Knights of Honor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(x => x = "Sir " + x)
+                                    .ToArray();
+
+            Action<string[]> print =
+                name => Console.WriteLine(string.Join(Environment.NewLine, name));
+
+            print(names);
+        }
+    }
+}
