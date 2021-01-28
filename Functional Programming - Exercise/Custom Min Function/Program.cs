cb5a011ace4cf43b2d62048f56415a0f73a84227
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-28 23:05:19 +0200

    Exercises

---
+++ Functional Programming - Exercise/Custom Min Function/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            Func<int[], int> func = 
+                x => x.Min();
+
+            Console.WriteLine(func(numbers));
+        }
+    }
+}
---
+++ Functional Programming - Exercise/Find Evens or Odds/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine()
+                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            string evenOrOdd = Console.ReadLine();
+
+            int start = range[0];
+            int end = range[1];
+
+            List<int> numbers = new List<int>();
+
+            for (int i = start; i <= end; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Predicate<int> predicate = x => true;
+
+            if (evenOrOdd == "even")
+            {
+                predicate = x => x % 2 == 0;
+            }
+            else if (evenOrOdd == "odd")
+            {
+                predicate = x => x % 2 != 0;
+            }
+
+            Action<List<int>> print =
+                x => Console.WriteLine(string.Join(" ", x.Where(x => predicate(x))));
+
+            print(numbers);
+
+        }
+    }
+}
