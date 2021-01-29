0afab4088bb3816e6836bcbadb1848cd5b2303b8
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-29 08:38:08 +0200

    Exercises

---
+++ Functional Programming - Exercise/Reverse and Exclude/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            List<int> divisibleNumbers = numbers.Reverse().ToList();
+
+            Predicate<int> predicate = x => x % n == 0;
+
+            Func<List<int>, List<int>> func =
+                nums => nums.Where(x => !(predicate(x))).ToList();
+
+            Action<List<int>> action = nums => Console.WriteLine(string.Join(" ", func(nums)));
+
+            action(divisibleNumbers);
+            
+        }
+    }
+}
