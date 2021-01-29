3557518bbf8eabff21a4053b1b7bd63c90a48080
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-29 22:58:24 +0200

    lab

---
+++ Defining Classes - Lab/Car Constructors/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Car_Constructors
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+    }
+
+
+}
