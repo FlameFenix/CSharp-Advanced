e9e889e222b66ab41d198f5895df0e5de3b49d1f
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-29 22:10:32 +0200

    lab

---
+++ Defining Classes - Lab/Car Extension/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car_Extension
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            // if the car fuel quantity minus the distance multiplied by the car fuel consumption is bigger than zero
+            if(FuelQuantity - distance * FuelConsumption >= 0)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+
+
+}
---
+++ Defining Classes - Lab/Car Extension/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Car_Extension;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car newCar = new Car();
+            newCar.Make = "VW";
+            newCar.Model = "MK3";
+            newCar.Year = 1992;
+            newCar.FuelConsumption = 200;
+            newCar.FuelQuantity = 200;
+            newCar.Drive(2000);
+            Console.WriteLine(newCar.WhoAmI());
+        }
+    }
+}
