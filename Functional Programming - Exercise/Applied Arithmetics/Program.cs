8446ed369397e643b90e8fb5dac46ea9ada32463
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-29 08:18:34 +0200

    Exercises

---
+++ Functional Programming - Exercise/Applied Arithmetics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            string command = Console.ReadLine();
+
+            Func<int[], int[]> func;
+
+            Action<int[]> print = null;
+
+            while (command != "end")
+            {
+                if (command == "add")
+                {
+                    func = num => num.Select(x => x + 1).ToArray();
+                    numbers = func(numbers);
+                }
+                else if (command == "multiply")
+                {
+                    func = num => num.Select(x => x * 2).ToArray();
+                    numbers = func(numbers);
+                }
+                else if (command == "subtract")
+                {
+                    func = num => num.Select(x => x - 1).ToArray();
+                    numbers = func(numbers);
+                }
+                else if (command == "print")
+                {
+                    print = num => Console.WriteLine(string.Join(" ", num));
+                    print(numbers);
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
