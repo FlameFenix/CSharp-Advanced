34471c891b7fef374bc41336763ce86b34d6c621
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-29 21:53:11 +0200

    Lab

---
+++ Defining Classes - Lab/Car/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car newCar = new Car();
+
+            newCar.Make = "VW";
+            newCar.Model = "MK3";
+            newCar.Year = 1992;
+
+            Console.WriteLine($"Make: {newCar.Make}\nModel: {newCar.Model}\nYear: {newCar.Year}");
+        }
+    }
+}
