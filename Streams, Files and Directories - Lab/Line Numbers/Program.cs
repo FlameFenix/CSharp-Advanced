8e2e4bc7245d9577f9391128a57ea6a341ca6756
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-30 21:18:57 +0200

    Lab

---
+++ Streams, Files and Directories - Lab/Line Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathInput = "../../../input.txt";
+            string pathOutput = "../../../output.txt";
+            StreamReader reader = new StreamReader(pathInput);
+
+            string currentLine = reader.ReadLine();
+
+            int counter = 1;
+                using (StreamWriter writer = new StreamWriter(pathOutput))
+                {
+                while (currentLine != null)
+                {
+                    writer.WriteLine($"{counter}. {currentLine}");
+                    currentLine = reader.ReadLine();
+                    counter++;
+                }
+            }
+        }
+    }
+}
---
+++ Streams, Files and Directories - Lab/Odd Lines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                string currentRow = reader.ReadLine();
+
+                int counter = 0;
+
+                while (currentRow != null)
+                {
+                    counter++;
+
+                    if(counter % 2 == 0)
+                    {
+                        Console.WriteLine(currentRow);                        
+                    }
+
+                    currentRow = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
