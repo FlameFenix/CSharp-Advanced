f3e29aabdb5357ec851cc59b56d61a4a124d4cbc
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-31 12:43:39 +0200

    Lab

---
+++ Streams, Files and Directories - Lab/Slice a File/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Slice_a_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(FileStream streamReader = new FileStream("../../../slice.txt", FileMode.Open))
+            {
+                int partSize = (int)streamReader.Length / 4;
+
+                for (int i = 0; i < streamReader.Length; i++)
+                {
+                    byte[] newbyte = new byte[1];
+                    int count = 0;
+                    
+                    using(FileStream streamWritter = new FileStream($"../../../slice-part{i +1}.txt", FileMode.Create, FileAccess.Write))
+                    {
+                        while (count < partSize)
+                        {
+                            streamReader.Read(newbyte, 0, newbyte.Length);
+                            streamWritter.Write(newbyte, 0, newbyte.Length);
+
+                            count += newbyte.Length;
+                        }
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
