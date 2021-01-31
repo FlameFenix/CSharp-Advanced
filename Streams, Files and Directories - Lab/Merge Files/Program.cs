459c5f3a91cad5075ecf796147f0d52f08eb10b3
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-31 11:32:59 +0200

    Lab

---
+++ Streams, Files and Directories - Lab/Merge Files/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader inputOne = new StreamReader("../../../input1.txt");
+
+            StreamReader inputTwo = new StreamReader("../../../input2.txt");
+
+            using(inputOne)
+            {
+                using(inputTwo)
+                {
+                    List<int> numbers = new List<int>();
+
+                    string firstLine = inputOne.ReadLine();
+                    string secondLine = inputTwo.ReadLine();
+
+                    while (firstLine != null && secondLine != null)
+                    {
+                        if (firstLine != null)
+                        {
+                            numbers.Add(int.Parse(firstLine));
+                            firstLine = inputOne.ReadLine();
+                        }
+                        if (secondLine != null)
+                        {
+                            numbers.Add(int.Parse(secondLine));
+                            secondLine = inputTwo.ReadLine();
+                        }
+                    }
+
+                    StreamWriter writer = new StreamWriter("../../../output.txt");
+
+                    using (writer)
+                    {
+                        foreach (var item in numbers)
+                        {
+                            writer.WriteLine(item);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
