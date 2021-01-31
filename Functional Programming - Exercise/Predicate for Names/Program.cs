9e59c00a23f2169039564ca2b48e6163748047fb
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-01-31 19:36:54 +0200

    Exercises

---
+++ Functional Programming - Exercise/Predicate for Names/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSymbols = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Where(x => x.Length <= numberOfSymbols)
+                                    .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
