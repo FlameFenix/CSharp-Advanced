b3d7c1e50e4bcfa92fef5891d84e3ceea77187fc
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-02 21:22:18 +0200

    Feeling excited

---
+++ Defining Classes - Lab/Car Engine and Tires/Car.cs
@@ -0,0 +1,66 @@
+﻿using CarManufacturer;
+using System;
+using System.Collections.Generic;
+
+namespace Car_Constructors
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+            Engine = engine;
+            Tires = tires;
+
+    }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+
+
+    }
+
+
+}
---
+++ Defining Classes - Lab/Car Engine and Tires/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using Car_Constructors;
+using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var tires = new Tire[4]{
+            new Tire(1, 2.5),
+            new Tire(1, 2.1),
+            new Tire(1, 0.5),
+            new Tire(2, 2.3),
+            };
+
+            var engine = new Engine(560, 6300);
+
+            var car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+
+            Console.WriteLine($"{car.Make} => {car.Model} => {car.Year} => {car.FuelQuantity} => {car.FuelConsumption} => HP {car.Engine.HorsePower} => CC {car.Engine.CubicCapacity} ");
+        }
+    }
+}
