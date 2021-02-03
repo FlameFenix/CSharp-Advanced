6a4ac2fc419ac50de365ef326a5ad8e2a48ed053
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-03 21:50:59 +0200

    Exercises

---
+++ Defining Classes - Exercise/PokemonTrainer/StartUp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+
+            List<Trainer> Trainers = new List<Trainer>();
+
+            while ((command = Console.ReadLine()) != "Tournament")
+            {
+                string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                          .ToArray();
+
+                string trainerName = cmdArgs[0];
+                string pokemonName = cmdArgs[1];
+                string pokemonElement = cmdArgs[2];
+                int pokemonHealth = int.Parse(cmdArgs[3]);
+
+                Pokemon currentPokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                Trainer isTrainerExist = Trainers.FirstOrDefault(x => x.Name == trainerName);
+                if(Trainers.Contains(isTrainerExist))
+                {
+                    isTrainerExist.Pokemons.Add(currentPokemon);
+                }
+                else
+                {
+                    Trainer currentTrainer = new Trainer(trainerName, 0, currentPokemon);
+                    Trainers.Add(currentTrainer);
+                }
+            }
+
+            string secondCommand = string.Empty;
+
+            while ((secondCommand = Console.ReadLine()) != "End")
+            {
+                string currentElement = secondCommand;
+
+                foreach (var trainer in Trainers)
+                {
+                    bool isElementExist = false;
+
+                    foreach (var pokemon in trainer.Pokemons)
+                    {
+                        if(pokemon.Element == currentElement)
+                        {
+                            isElementExist = true;
+                        }
+                    }
+
+                    if(isElementExist)
+                    {
+                        trainer.Badges += 1;
+                    }
+                    else
+                    {
+                        RemovePokemon(trainer);
+                    }
+                }
+            }
+
+            foreach (var trainer in Trainers.OrderByDescending(x => x.Badges))
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.Badges} {trainer.Pokemons.Count}");
+            }
+        }
+
+        private static void RemovePokemon(Trainer trainer)
+        {
+            for (int i = 0; i < trainer.Pokemons.Count; i++)
+            {
+                trainer.Pokemons[i].Health -= 10;
+
+                if (trainer.Pokemons[i].Health <= 0)
+                {
+                    trainer.Pokemons.Remove(trainer.Pokemons[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Defining Classes - Exercise/PokemonTrainer/Trainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name, int badges, Pokemon pokemon)
+        {
+            Name = name;
+            Badges = badges;
+            Pokemons = new List<Pokemon>() { pokemon };
+        }
+
+        public string Name { get; set; }
+
+        public int Badges { get; set; }
+
+        public List<Pokemon> Pokemons { get; set; }
+    }
+}
