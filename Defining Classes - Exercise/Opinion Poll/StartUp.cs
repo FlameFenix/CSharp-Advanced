ac266e37d48e8ff65751768cb21fdef2709f14a0
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-03 11:26:02 +0200

    Exercises

---
+++ Defining Classes - Exercise/Opinion Poll/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> listOfMembers = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine()
+                                          .Split()
+                                          .ToArray();
+
+                string name = cmdArgs[0];
+                int age = int.Parse(cmdArgs[1]);
+
+                Person newPerson = new Person(name, age);
+
+                listOfMembers.Add(newPerson);
+            }
+
+            foreach (var item in listOfMembers.Where(x => x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+        }
+    }
+}
