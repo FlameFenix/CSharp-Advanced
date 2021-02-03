9330306f76fd461ef1e1b9707e05df6f09f30276
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-03 12:05:37 +0200

    Exercises

---
+++ Defining Classes - Exercise/DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifierProblem
+{
+    public class DateModifier
+    {
+        public static int DiffrenceBetweenDates(string firstDate, string secondDate)
+        {
+            DateTime dateOne = DateTime.Parse(firstDate);
+            DateTime dateTwo = DateTime.Parse(secondDate);
+
+            TimeSpan diffrence = dateOne - dateTwo;
+
+            return Math.Abs(diffrence.Days);
+        }
+    }
+}
