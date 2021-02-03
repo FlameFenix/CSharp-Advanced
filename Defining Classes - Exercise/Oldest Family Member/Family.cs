9dfd60f0ed801156e5800b1e02d59cea3731ad7e
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-03 11:17:31 +0200

    Exercises

---
+++ Defining Classes - Exercise/Oldest Family Member/Family.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            FamilyMembers = new List<Person>();
+        }
+
+        public List<Person> FamilyMembers { get; set; }
+
+        public void AddMember(Person member)
+        {
+            FamilyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestPerson = FamilyMembers.OrderByDescending(x => x.Age).First();
+            return oldestPerson;
+        }
+    }
+}
---
+++ Defining Classes - Exercise/Oldest Family Member/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family familyMembers = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine()
+                                          .Split()
+                                          .ToArray();
+
+                string name = cmdArgs[0];
+                int age = int.Parse(cmdArgs[1]);
+
+                Person newFamilyMember = new Person(name, age);
+
+                familyMembers.AddMember(newFamilyMember);
+
+            }
+            Console.WriteLine($"{familyMembers.GetOldestMember().Name} {familyMembers.GetOldestMember().Age}");
+        }
+    }
+}
