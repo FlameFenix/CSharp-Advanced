23e33411c13aab1d05988900f3c09f399116a0bd
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-05 22:06:52 +0200

    Lab

---
+++ Generics - Lab/BoxOfT/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            BoxOfT<int> generic = new BoxOfT<int>();
+
+            generic.Add(1);
+            generic.Add(2);
+            generic.Add(3);
+
+            Console.WriteLine(generic.Remove());
+
+            Console.WriteLine(generic.Count);
+
+            Console.WriteLine(generic.Remove());
+
+            Console.WriteLine(generic.Count);
+
+        }
+    }
+}
