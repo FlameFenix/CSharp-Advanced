dfdfffa8d0c7c1f6e69ea017aaabbbd5f61d7d3e
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-05 22:58:46 +0200

    Lab

---
+++ Generics - Lab/ArrayCreator/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = ArrayCreator.Create(10, 10);
+
+            Console.WriteLine(string.Join(" ", arr));
+
+            string[] array2 = ArrayCreator.Create(5, "Cheaters");
+
+            Console.WriteLine(string.Join(", ", array2));
+        }
+    }
+}
