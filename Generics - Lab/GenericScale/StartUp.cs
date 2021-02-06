afe305c5899fa87592dce9742936239999e3fff5
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-06 22:26:53 +0200

    Lab

---
+++ Generics - Lab/GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> numbers = new EqualityScale<int>(10, 10);
+
+            Console.WriteLine(numbers.AreEqual());
+        }
+    }
+}
