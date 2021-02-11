51751b4ac2bb690ce41748aa378dfedd4d9bae32
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-11 23:21:02 +0200

    Exercises

---
+++ Generics - Exercise/GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,33 @@
+﻿using GenericSwapMethodStrings;
+using System;
+using System.Linq;
+
+namespace GenericSwapMethodIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            GenericMethod<int> generic = new GenericMethod<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                int number = int.Parse(Console.ReadLine());
+
+                generic.list.Add(number);
+            }
+
+            int[] indexes = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            generic.Swap(indexes[0], indexes[1]);
+
+            Console.WriteLine(generic.ToString());
+        }
+    }
+}
