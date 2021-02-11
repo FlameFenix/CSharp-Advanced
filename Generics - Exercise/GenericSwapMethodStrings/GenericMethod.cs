ceda4af4fc8f66ab91974fb3a93adc91d43a6434
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-11 23:01:55 +0200

    Exercises

---
+++ Generics - Exercise/GenericSwapMethodStrings/GenericMethod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    public class GenericMethod<T>
+    {
+
+        public GenericMethod()
+        {
+            list = new List<T>();
+        }
+
+        public List<T> list { get; set; }
+
+        public List<T> Swap(int firstIndex, int secondIndex)
+        {
+            T firstElement = list[firstIndex];
+            T secondElement = list[secondIndex];
+
+            list[firstIndex] = secondElement;
+            list[secondIndex] = firstElement; 
+
+            return list;
+        }
+
+        public override string ToString()
+        {
+            string output = string.Empty;
+
+            foreach (var item in list)
+            {
+                output += $"{item.GetType()}: {item}" + Environment.NewLine;
+            }
+
+            return output.Trim();
+        }
+    }
+}
---
+++ Generics - Exercise/GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            GenericMethod<string> generic = new GenericMethod<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string name = Console.ReadLine();
+
+                generic.list.Add(name);
+            }
+
+            int[] indexes = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            generic.Swap(indexes[0], indexes[1]);
+
+            Console.WriteLine(generic.ToString());
+        }
+    }
+}
