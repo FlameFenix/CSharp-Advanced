2cfbcb0b5d2b2ba8493f4ab7410be30fcf7a173c
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-14 18:03:10 +0200

    Exercises

---
+++ Generics - Exercise/GenericCountMethodString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GenericCountMethodString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+
+            Box<string> newBox = new Box<string>();
+            
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                string element = Console.ReadLine();
+
+                newBox.Add(element);
+            }
+
+            string comparableElement = Console.ReadLine();
+
+            Console.WriteLine(newBox.CompareByValue(comparableElement));
+        }
+    }
+}
