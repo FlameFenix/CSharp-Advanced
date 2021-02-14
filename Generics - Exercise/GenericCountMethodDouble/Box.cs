be9822e208620dc4a9797c1a16e450d253617461
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-14 18:12:55 +0200

    Exercises

---
+++ Generics - Exercise/GenericCountMethodDouble/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable
+    {
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        private List<T> list;
+
+        public int Count { get; set; }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public int CompareByValue(T element)
+        {
+            double currentElement = double.Parse(element.ToString());
+
+            foreach (var value in list)
+            {
+                if(value.CompareTo(currentElement) > 0)
+                {
+                    Count++;
+                }
+            }
+
+            return Count;
+        }
+    }
+}
---
+++ Generics - Exercise/GenericCountMethodDouble/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GenericCountMethodDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+
+            Box<double> newBox = new Box<double>();
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                double element = double.Parse(Console.ReadLine());
+
+                newBox.Add(element);
+            }
+
+            double comparableElement = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(newBox.CompareByValue(comparableElement));
+        }
+    }
+}
