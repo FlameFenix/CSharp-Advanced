e8644a052932c0812fff363afdcbfa2e709ed183
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-15 23:38:31 +0200

    Exercises

---
+++ Basic Algorithms - Exercise/Recursive Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial = int.Parse(Console.ReadLine());
+            int result = Factorial(factorial);
+            Console.WriteLine(result);
+        }
+
+        private static int Factorial(int num)
+        {
+            if(num == 0)
+            {
+                num = 1;
+                return num;
+            }
+
+            return num *= Factorial(num - 1);
+        }
+    }
+}
