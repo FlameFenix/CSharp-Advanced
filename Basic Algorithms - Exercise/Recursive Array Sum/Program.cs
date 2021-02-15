1ef2b4bf6127fdb777e95684aed8442e5ceb27f5
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-15 23:22:59 +0200

    Exercises

---
+++ Basic Algorithms - Exercise/Recursive Array Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Recursive_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                                 .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int sum = Sum(array, 0);
+
+            Console.WriteLine(sum);
+        }
+
+        private static int Sum(int[] array, int index)
+        {
+            int sum = 0;
+            
+            if(index == array.Length - 1)
+            {
+                return array[index];
+            }
+
+            return array[index] + Sum(array, index + 1);
+        }
+    }
+    
+}
