5c8bb026ab51b7ceac0750b2f91aadcaa788ebb4
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-19 10:49:14 +0200

    Exercises

---
+++ Generics - Exercise/ThreeupleTask/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace ThreeupleTask
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] personInfoOne = Console.ReadLine()
+                                            .Split()
+                                            .ToArray();
+
+            string fullName = $"{personInfoOne[0]} {personInfoOne[1]}";
+            string adress = $"{personInfoOne[2]}";
+            string town = $"{personInfoOne[3]}";
+
+            Threeuple<string, string, string> firstThreeuple = new Threeuple<string, string, string>(fullName, adress, town);
+
+            string[] personInfoTwo = Console.ReadLine()
+                                            .Split()
+                                            .ToArray();
+
+            string name = $"{personInfoTwo[0]}";
+            int litters = int.Parse(personInfoTwo[1]);
+            bool isDrunk = personInfoTwo[2] == "drunk" ? true : false;
+
+            Threeuple<string, int, bool> secondThreeuple = new Threeuple<string, int, bool>(name, litters, isDrunk);
+
+            string[] personInfoThree = Console.ReadLine()
+                                              .Split()
+                                              .ToArray();
+
+            string balanceOwner = $"{personInfoThree[0]}";
+            double balance = double.Parse(personInfoThree[1]);
+            string bankName = $"{personInfoThree[2]}";
+
+            Threeuple<string, double, string> thirdThreeuple = new Threeuple<string, double, string>(balanceOwner, balance, bankName);
+
+            Console.WriteLine(firstThreeuple);
+            Console.WriteLine(secondThreeuple);
+            Console.WriteLine(thirdThreeuple);
+        }
+    }
+}
---
+++ Generics - Exercise/ThreeupleTask/Threeuple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreeupleTask
+{
+    class Threeuple<Tfirst, Tsecond, Tthird>
+    {
+        public Tfirst FirstElement { get; set; }
+
+        public Tsecond SecondElement { get; set; }
+
+        public Tthird ThirdElement { get; set; }
+
+        public Threeuple(Tfirst first, Tsecond second, Tthird third)
+        {
+            FirstElement = first;
+            SecondElement = second;
+            ThirdElement = third;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstElement} -> {SecondElement} -> {ThirdElement}";
+        }
+    }
+}
