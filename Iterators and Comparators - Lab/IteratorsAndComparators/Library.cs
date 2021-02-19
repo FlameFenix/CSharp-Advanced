1c5a827cc16c572558ecdc165c5f30989896ac44
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-19 14:19:36 +0200

    Lab

---
+++ Iterators and Comparators - Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public List<Book> BooksList { get; set; }
+
+        public Library(params Book[] books)
+        {
+            BooksList = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return BooksList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
