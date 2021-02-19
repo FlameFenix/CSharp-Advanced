5db69cf74ac7afe4f6d3e31f8ec628ef215b1c64
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-19 19:07:33 +0200

    Exercises

---
+++ Iterators and Comparators - Exercise/ListyIteratorTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIteratorTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+
+            ListyIterator<string> listy = new ListyIterator<string>();
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = command.Split().ToArray();
+
+                switch (cmdArgs[0])
+                {
+                    case "Create":
+
+                        List<string> items = cmdArgs.Skip(1).ToList();
+
+                        listy.Create(items);
+
+                        break;
+                    case "Print":
+                        listy.Print();
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listy.HasNext());
+                        break;
+                    case "Move":
+                        Console.WriteLine(listy.MoveNext());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
